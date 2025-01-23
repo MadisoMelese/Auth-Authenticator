38c3b31b11c1446f13b46e91c799062d88ed5d54
Madiso Melese <madishamadiso00@gmail.com>
2025-01-23 22:54:04 +0300

    some update on authStore

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -34,4 +34,27 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
+  
+
+  login: async (email, password) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/login`, {
+        email,
+        password,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error logging in",
+        isLoading: false,
+      });
+      throw error;
+    }
+
+  },
 }));
