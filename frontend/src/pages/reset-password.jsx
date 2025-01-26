a325d6299c94b368c975e34ceb88ae232edff103
Madiso Melese <157389082+MadisoMelese@users.noreply.github.com>
2025-01-26 22:16:24 +0300

    Update reset-password.jsx

--- frontend/src/pages/reset-password.jsx
+++ frontend/src/pages/reset-password.jsx
@@ -67,18 +67,7 @@ const Login = () => {
             </Link>
           </div>
           {err && <p className="text-red-500 text-sm mb-4">{err}</p>}
-          <motion.button
-            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            type="submit"
-          >
-            {isLloading ? (
-              <Loader className="size-6 animate-spin mx-auto" />
-            ) : (
-              "Login"
-            )}
-          </motion.button>
+         
         </form>
       </div>
 
